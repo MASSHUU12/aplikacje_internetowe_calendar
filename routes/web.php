9bb67086dd8658b6f8c66cc4b13834eb34211190
MASSHUU12 <gawrysiak.maciej@proton.me>
2026-01-15 14:18:52 +0100

    feat(frontend): add basic pages

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,19 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('home');
 })->name('home');
+
+Route::get('/login', function () {
+    return Inertia::render('login');
+})->name('login');
+
+Route::get('/logout', function () {
+    return Inertia::render('logout');
+})->name('logout');
+
+Route::get('/register', function () {
+    return Inertia::render('register');
+})->name('register');
+
+Route::get('/dashboard', function () {
+    return Inertia::render('dashboard');
+})->name('dashboard');
